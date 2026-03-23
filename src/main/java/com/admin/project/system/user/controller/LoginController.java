58812d4bb9897cdce267e6cb9ef68b3152f21475
caicaigogo <cgl1991fzu@sina.com>
2026-03-23 15:16:51 +0800

    add remember me

--- src/main/java/com/admin/project/system/user/controller/LoginController.java
+++ src/main/java/com/admin/project/system/user/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.admin.project.system.user.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,17 @@ import com.admin.framework.web.controller.BaseController;
 @Controller
 public class LoginController extends BaseController
 {
+    /**
+     * 是否开启记住我功能
+     */
+    @Value("${shiro.rememberMe.enabled: false}")
+    private boolean rememberMe;
 
     @GetMapping("/login")
     public String login(HttpServletRequest request, HttpServletResponse response, ModelMap mmap)
     {
+        // 是否开启记住我
+        mmap.put("isRemembered", rememberMe);
         return "login";
     }
 }
