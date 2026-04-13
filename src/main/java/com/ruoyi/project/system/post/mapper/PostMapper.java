52a71fbb5b5f07ac3f0245d8d1386334df1b0ed1
caicaigogo <cgl1991fzu@sina.com>
2026-04-13 10:29:10 +0800

    add post part

--- src/main/java/com/ruoyi/project/system/post/mapper/PostMapper.java
+++ src/main/java/com/ruoyi/project/system/post/mapper/PostMapper.java
@@ -10,13 +10,13 @@ import com.ruoyi.project.system.post.domain.Post;
  */
 public interface PostMapper
 {
-//    /**
-//     * 查询岗位数据集合
-//     *
-//     * @param post 岗位信息
-//     * @return 岗位数据集合
-//     */
-//    public List<Post> selectPostList(Post post);
+    /**
+     * 查询岗位数据集合
+     *
+     * @param post 岗位信息
+     * @return 岗位数据集合
+     */
+    public List<Post> selectPostList(Post post);
 
     /**
      * 查询所有岗位
@@ -33,51 +33,51 @@ public interface PostMapper
      */
     public List<Post> selectPostsByUserId(Long userId);
 
-//    /**
-//     * 通过岗位ID查询岗位信息
-//     *
-//     * @param postId 岗位ID
-//     * @return 角色对象信息
-//     */
-//    public Post selectPostById(Long postId);
-//
-//    /**
-//     * 批量删除岗位信息
-//     *
-//     * @param ids 需要删除的数据ID
-//     * @return 结果
-//     */
-//    public int deletePostByIds(Long[] ids);
-//
-//    /**
-//     * 修改岗位信息
-//     *
-//     * @param post 岗位信息
-//     * @return 结果
-//     */
-//    public int updatePost(Post post);
-//
-//    /**
-//     * 新增岗位信息
-//     *
-//     * @param post 岗位信息
-//     * @return 结果
-//     */
-//    public int insertPost(Post post);
-//
-//    /**
-//     * 校验岗位名称
-//     *
-//     * @param postName 岗位名称
-//     * @return 结果
-//     */
-//    public Post checkPostNameUnique(String postName);
-//
-//    /**
-//     * 校验岗位编码
-//     *
-//     * @param postCode 岗位编码
-//     * @return 结果
-//     */
-//    public Post checkPostCodeUnique(String postCode);
+    /**
+     * 通过岗位ID查询岗位信息
+     *
+     * @param postId 岗位ID
+     * @return 角色对象信息
+     */
+    public Post selectPostById(Long postId);
+
+    /**
+     * 批量删除岗位信息
+     *
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deletePostByIds(Long[] ids);
+
+    /**
+     * 修改岗位信息
+     *
+     * @param post 岗位信息
+     * @return 结果
+     */
+    public int updatePost(Post post);
+
+    /**
+     * 新增岗位信息
+     *
+     * @param post 岗位信息
+     * @return 结果
+     */
+    public int insertPost(Post post);
+
+    /**
+     * 校验岗位名称
+     *
+     * @param postName 岗位名称
+     * @return 结果
+     */
+    public Post checkPostNameUnique(String postName);
+
+    /**
+     * 校验岗位编码
+     *
+     * @param postCode 岗位编码
+     * @return 结果
+     */
+    public Post checkPostCodeUnique(String postCode);
 }
